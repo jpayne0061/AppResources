cd886b21e3c2940bf08107e014ff0d269d4269d5
unknown <jpayne0061@gmail.com>
2019-11-10 08:16:49 -0500

    add base project

---
+++ AppResourcesWatcher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppResourcesWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
